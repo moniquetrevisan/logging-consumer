f3d403b59627d411f9738455e27afa1c60f17098
Monique Volpe <moniquevolpe@MacBook-Air-de-Monique.local>
2022-06-25 17:59:43 -0300

    first commit

---
+++ src/main/kotlin/com/example/loggingconsumer/LoggingConsumerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.loggingconsumer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoggingConsumerApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoggingConsumerApplication>(*args)
+}
